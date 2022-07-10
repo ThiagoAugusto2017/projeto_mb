36e68c0786793aedf48c879417a728e4e5ba6091
Thiago Augusto dos santos <96118290+ThiagoAugusto2017@users.noreply.github.com>
2022-07-09 22:34:23 -0300

    CHORE:cadastro e login

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,26 @@
 import {Router} from 'express';
-import {InputUser} from '../controller/userLogin';
+import {InputLogin} from '../controller/userLogin';
+import {InputUsuario} from '../controller/InputUsuario';
 import {validate} from '../middleware/handleValidation';
-import {userInputValidation} from '../middleware/userValidation';
+import {userInputValidation} from '../middleware/userLoginValidation';
+import authorization from '../middleware/auth';
+import {cadastroValidation} from '../middleware/cadastroValidation';
 
 const router = Router();
 
-router.post('/api/user', userInputValidation(), validate, InputUser.input);
+router.post(
+	'/api/login/cadastro',
+	userInputValidation(),
+	validate,
+	InputLogin.input,
+);
+router.post('/api/login', InputLogin.validateSession);
+
+router.post(
+	'/api/usuario',
+	cadastroValidation(),
+	authorization.verifyToken,
+	InputUsuario.usuario,
+);
 
 export default router;
